31d44f0abcf0772abf1948bea844e96799aa53ee
faith-goto <goto@faith.co.jp>
2020-01-09 16:30:04 +0900

    add: topics

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'home/index'
+  get 'topics/index'
+  get 'topics/show'
+  get 'topics/new'
+  get 'topics/edit'
+  resources :topics
 end
